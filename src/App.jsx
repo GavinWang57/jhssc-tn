cc666c02f376dcc09fd09ff013aa204598aadf73
冠翔 <crown.soar@gmail.com>
2026-04-12 00:33:16 +0800

    路由改用 HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Nav from "./components/layout/Nav";
 import Footer from "./components/layout/Footer";
 import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.PROD ? "/jhssc-tn/" : "/"}>
+    <HashRouter>
       <div className="d-flex flex-column min-vh-100">
         <Nav />
         <main className="flex-grow-1">
@@ -13,7 +13,7 @@ function App() {
         </main>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
