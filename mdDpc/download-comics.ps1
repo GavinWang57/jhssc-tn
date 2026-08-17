7c7ef133594d6439fd1975e8c8fe71550037fe24
冠翔 <crown.soar@gmail.com>
2026-08-17 22:35:04 +0800

    職群漫畫，圖片部分修改

---
+++ mdDpc/download-comics.ps1
@@ -0,0 +1,192 @@
+﻿$ErrorActionPreference = "Stop"
+
+$destDir = Join-Path (Split-Path $PSScriptRoot -Parent) "src\assets\images\OtherResources"
+if (-not (Test-Path $destDir)) {
+    New-Item -ItemType Directory -Path $destDir | Out-Null
+}
+
+$groups = @(
+    @{ Slug = "home-economics"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-1.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-2.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-3.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-4.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-5.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-6.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-7.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/1.家政職群-8.jpg"
+    ) },
+    @{ Slug = "electronics-electrical"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-1.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-2.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-3.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-4.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-5.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-6.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-7.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/2.電子與電機職群-8.jpg"
+    ) },
+    @{ Slug = "arts"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-1.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-2.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-3.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-4.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-5.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-6.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-7.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/3.藝術職群-8.jpg"
+    ) },
+    @{ Slug = "health-care"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-1-scaled.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-2-scaled.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-3-scaled.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-4-scaled.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-5-scaled.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-6-scaled.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-7-scaled.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2025/05/4.醫護職群-8-scaled.jpg"
+    ) },
+    @{ Slug = "power-mechanical"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-01.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-02.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-03.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-04.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-05.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-06.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-07.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_動力機械-08.png"
+    ) },
+    @{ Slug = "business-management"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-01.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-02.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-03.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-04.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-05.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-06.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-07.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_商管-08.png"
+    ) },
+    @{ Slug = "chemical-engineering"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-01.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-02.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-03.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-04.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-05.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-06.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-07.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_化工-08.png"
+    ) },
+    @{ Slug = "mechanical"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-01.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-02.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-03.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-04.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-05.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-06.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-07.png",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2022/06/漫畫_機械-08.png"
+    ) },
+    @{ Slug = "maritime"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_海事-08.jpg"
+    ) },
+    @{ Slug = "fisheries"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_水產-08.jpg"
+    ) },
+    @{ Slug = "foreign-language"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_外語-08.jpg"
+    ) },
+    @{ Slug = "hospitality-tourism"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2023/05/漫畫_餐旅-08.jpg"
+    ) },
+    @{ Slug = "agriculture"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_農業-08.jpg"
+    ) },
+    @{ Slug = "design"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_設計-08.jpg"
+    ) },
+    @{ Slug = "food"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_食品-08.jpg"
+    ) },
+    @{ Slug = "civil-construction"; Urls = @(
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-01.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-02.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-03.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-04.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-05.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-06.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-07.jpg",
+        "https://test.camping.org.tw/wordpress/hvac/wp-content/uploads/2024/05/漫畫_土木建築-08.jpg"
+    ) }
+)
+
+$headers = @{ "User-Agent" = "Mozilla/5.0" }
+$mapping = @{}
+
+foreach ($group in $groups) {
+    $slug = $group.Slug
+    $urls = $group.Urls
+    $localNames = @()
+    for ($i = 0; $i -lt $urls.Count; $i++) {
+        $url = $urls[$i]
+        $ext = [System.IO.Path]::GetExtension($url)
+        $num = "{0:D2}" -f ($i + 1)
+        $fileName = "$slug-$num$ext"
+        $outPath = Join-Path $destDir $fileName
+        Invoke-WebRequest -Uri $url -OutFile $outPath -Headers $headers
+        $localNames += $fileName
+        Write-Host "Downloaded $fileName"
+    }
+    $mapping[$slug] = $localNames
+}
+
+Write-Host "All downloads complete."
+
--- src/pages/OtherResources.jsx
+++ src/pages/OtherResources.jsx
@@ -1,8 +1,207 @@
-function OtherResources() {
+import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import "../assets/scss/pages/_other-resources.scss";
+import otherResourcesData from "../data/otherResources.json";
+
+const { sections } = otherResourcesData;
+
+// 職群漫畫圖片實際存放於 assets，JSON 只存檔名，需在此對應成建置後路徑
+const comicImageModules = import.meta.glob(
+  "../assets/images/OtherResources/*.{jpg,png}",
+  { eager: true, import: "default" },
+);
+
+function resolveComicImage(fileName) {
+  const entry = Object.entries(comicImageModules).find(([path]) =>
+    path.endsWith(`/${fileName}`),
+  );
+  return entry ? entry[1] : "";
+}
+
+const SITE_NAME = "臺南市國民中學生涯及技藝教育資源網";
+
+// 資源檢視彈窗，依 type 顯示 PDF 預覽、YouTube 影片或漫畫圖片捲動清單
+function ResourceModal({ resource, onClose }) {
+  useEffect(() => {
+    if (!resource) return;
+
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") onClose();
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    document.body.classList.add("modal-open");
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.body.classList.remove("modal-open");
+    };
+  }, [resource, onClose]);
+
+  if (!resource) return null;
+
   return (
-    <div className="container py-5">
-      <div className="h1">其他資源頁面</div>
-    </div>
+    <>
+      <div
+        className="modal fade show other-resources__modal"
+        style={{ display: "block" }}
+        tabIndex={-1}
+        role="dialog"
+        aria-modal="true"
+        onClick={onClose}
+      >
+        <div
+          className="modal-dialog modal-dialog-centered modal-lg"
+          onClick={(event) => event.stopPropagation()}
+        >
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{resource.name}</h5>
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={onClose}
+              ></button>
+            </div>
+            <div className="modal-body p-0">
+              {resource.type === "pdf" && (
+                <iframe
+                  className="other-resources__pdf-frame"
+                  src={`https://drive.google.com/file/d/${resource.fileId}/preview`}
+                  title={resource.name}
+                  allow="autoplay"
+                ></iframe>
+              )}
+              {resource.type === "video" && (
+                <div className="other-resources__player">
+                  <iframe
+                    src={`https://www.youtube.com/embed/${resource.videoId}?autoplay=1&rel=0`}
+                    title={resource.name}
+                    allow="autoplay; encrypted-media; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              )}
+              {resource.type === "comic" && (
+                <div className="other-resources__comic-scroll">
+                  {resource.images.map((fileName) => (
+                    <img
+                      src={resolveComicImage(fileName)}
+                      alt={resource.name}
+                      key={fileName}
+                      loading="lazy"
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal-backdrop fade show"></div>
+    </>
+  );
+}
+
+// 單一檔案資源卡片（PDF 名單 / 宣導動畫），橫式呈現名稱與更新頻率
+function FileResourceCard({ icon, name, note, onSelect }) {
+  return (
+    <button
+      type="button"
+      className="other-resources__file-card"
+      onClick={onSelect}
+    >
+      <span className="other-resources__file-icon">
+        <i className={`bi ${icon}`}></i>
+      </span>
+      <span className="other-resources__file-body">
+        <span className="other-resources__file-name">{name}</span>
+        <span className="other-resources__file-note">{note}</span>
+      </span>
+      <i className="bi bi-chevron-right other-resources__file-arrow"></i>
+    </button>
+  );
+}
+
+// 職群漫畫卡片：以壓縮縮圖顯示，點選後才載入完整漫畫圖片
+function ComicCard({ comic, onSelect }) {
+  return (
+    <button type="button" className="other-resources__card" onClick={onSelect}>
+      <span className="other-resources__card-thumb">
+        <img
+          src={resolveComicImage(comic.thumb)}
+          alt={comic.name}
+          loading="lazy"
+        />
+      </span>
+      <span className="other-resources__card-body">
+        <span className="other-resources__card-name">{comic.name}</span>
+        <span className="other-resources__card-cta">
+          <i className="bi bi-book-fill"></i> 查看漫畫
+        </span>
+      </span>
+    </button>
+  );
+}
+
+function OtherResources() {
+  const [activeResource, setActiveResource] = useState(null);
+
+  return (
+    <>
+      <Helmet>
+        <title>其他資源 | {SITE_NAME}</title>
+      </Helmet>
+
+      <div className="container py-5 other-resources">
+        <h1 className="h1 other-resources__title mb-4">其他資源</h1>
+
+        {sections.map((section, index) => (
+          <section
+            className={`other-resources__section${
+              index < sections.length - 1 ? " mb-5" : ""
+            }`}
+            key={section.id}
+          >
+            <h2 className="h4 other-resources__section-title mb-3">
+              {section.title}
+              {section.note && (
+                <span className="other-resources__section-note">
+                  {" "}
+                  （{section.note}）
+                </span>
+              )}
+            </h2>
+
+            {section.layout === "file" && (
+              <FileResourceCard
+                icon={section.icon}
+                name={section.resource.name}
+                note={section.resource.note}
+                onSelect={() => setActiveResource(section.resource)}
+              />
+            )}
+
+            {section.layout === "grid" && (
+              <div className="other-resources__card-grid">
+                {section.items.map((item) => (
+                  <ComicCard
+                    comic={item}
+                    key={item.name}
+                    onSelect={() => setActiveResource(item)}
+                  />
+                ))}
+              </div>
+            )}
+          </section>
+        ))}
+      </div>
+
+      <ResourceModal
+        resource={activeResource}
+        onClose={() => setActiveResource(null)}
+      />
+    </>
   );
 }
 
